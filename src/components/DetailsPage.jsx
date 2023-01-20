17056804efd86b5c9cad5fe05762e1af9c42b56a
Simone Backes <88430766+sihbackes@users.noreply.github.com>
2023-01-20 10:15:06 +0000

    detailspage

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -2,6 +2,7 @@ import {  useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux"
 import { useEffect} from "react";
 import { getImageByIdAction } from "../redux/actions";
+import { Container } from "react-bootstrap";
 
 const DetailsPage = () => {
   const data = useSelector((state) => state.image.dataImage)
@@ -13,7 +14,28 @@ const DetailsPage = () => {
   useEffect(() => {
    dispatch(getImageByIdAction(id))
   },[]);
-  console.log(data.hits)
+  console.log(data)
+  const image = data.hits[0]
+
+
+  return(
+    <>
+  
+    <Container>
+      <div>
+      <img  src={image.webformatURL} alt="" />
+      </div>
+      <div>
+        <p>Likes ({image.likes})</p>
+        <p>Tags ({image.tags})</p>
+        <p>Downloads ({image.downloads})</p>
+      </div>
+    
+    </Container>
+
+    </>
+    
+  )
 }
 
 export default DetailsPage
