5b9f4cd6f52e597ef3595c699fa721b889f47bcf
Simone Backes <88430766+sihbackes@users.noreply.github.com>
2023-01-24 19:21:06 +0000

    test deploy

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,16 +1,17 @@
+import { useEffect} from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux"
-import { useEffect} from "react";
+
 import { getImageByIdAction } from "../redux/actions";
-import { addToFavoritesAction } from "../redux/actions";
-import { Container } from "react-bootstrap";
-import NavSearchBar from "./NavSearchBar";
-import Footer from "./FooterComponent";
-import {AiOutlineTag, AiFillHeart, AiOutlineHeart, AiOutlineDownload} from 'react-icons/ai'
+// import { addToFavoritesAction } from "../redux/actions";
+// import { Container } from "react-bootstrap";
+// import NavSearchBar from "./NavSearchBar";
+// import Footer from "./FooterComponent";
+// import {AiOutlineTag, AiFillHeart, AiOutlineHeart, AiOutlineDownload} from 'react-icons/ai'
 
 const DetailsPage = () => {
   const data = useSelector((state) => state.image.dataImage)
-  const favorites = useSelector((state) => state.favorites.content)
+  // const favorites = useSelector((state) => state.favorites.content)
   const dispatch = useDispatch();
   const params = useParams();
   let id = params.id
@@ -23,14 +24,14 @@ const DetailsPage = () => {
   }, [dispatch, id]);
 
 
-  const handleFavorites = (id) => {
-    const find = favorites.find(element => element.id === id)
-    if(find){
-     return true
-    } else{
-     return false
-    }
-   }
+  // const handleFavorites = (id) => {
+  //   const find = favorites.find(element => element.id === id)
+  //   if(find){
+  //    return true
+  //   } else{
+  //    return false
+  //   }
+  //  }
 
 
   
@@ -38,44 +39,50 @@ const DetailsPage = () => {
     return "loading...."
   }
   
-  const image = data.hits[0]
-
-
+  // const image = data.hits[0]
+  
   return(
-    <>
-     <NavSearchBar/>
-      <Container>
-      <div className="main-detail-div mt-5 mb-5">
-       <div>
-         <img className="image-detail" src={image.webformatURL} alt="" />
-       </div>
-       <div className="image-detail-info">
-         <div>
-           <a href={`https://pixabay.com/users/${image.user}-${image.user_id}/`} target="_blank" rel="noreferrer">
-             <div className="user-info">
-                <img className="user-image" src={image.userImageURL} alt="" />
+    <div>
+      Test
+    </div>
+  )
+
+
+  // return(
+  //   <>
+  //    <NavSearchBar/>
+  //     <Container>
+  //     <div className="main-detail-div mt-5 mb-5">
+  //      <div>
+  //        <img className="image-detail" src={image.webformatURL} alt="" />
+  //      </div>
+  //      <div className="image-detail-info">
+  //        <div>
+  //          <a href={`https://pixabay.com/users/${image.user}-${image.user_id}/`} target="_blank" rel="noreferrer">
+  //            <div className="user-info">
+  //               <img className="user-image" src={image.userImageURL} alt="" />
                 
-                 <div className="user-info-p"><strong>{image.user}</strong></div>
-             </div>
-           </a>
-         </div>
-         <div className="p-2 mt-5" onClick={() => { dispatch(addToFavoritesAction(image))}}>
-            {handleFavorites(image.id)? <AiFillHeart  size={22} className="icon red mr-2"/>: <AiOutlineHeart size={22} className="icon mr-2"/>} 
-            Add to Favourites 
-         </div>
-         <div className="p-2"> <AiOutlineTag className="mr-2" size={24}/>{image.tags}</div>
-         <div className="p-2 mt-5">
-          <a href={image.pageURL} target="_blank" rel="noreferrer"><button className="download-btn">Download <AiOutlineDownload size={23}/></button></a>
-         </div>
-       </div>
-      </div>
+  //                <div className="user-info-p"><strong>{image.user}</strong></div>
+  //            </div>
+  //          </a>
+  //        </div>
+  //        <div className="p-2 mt-5" onClick={() => { dispatch(addToFavoritesAction(image))}}>
+  //           {handleFavorites(image.id)? <AiFillHeart  size={22} className="icon red mr-2"/>: <AiOutlineHeart size={22} className="icon mr-2"/>} 
+  //           Add to Favourites 
+  //        </div>
+  //        <div className="p-2"> <AiOutlineTag className="mr-2" size={24}/>{image.tags}</div>
+  //        <div className="p-2 mt-5">
+  //         <a href={image.pageURL} target="_blank" rel="noreferrer"><button className="download-btn">Download <AiOutlineDownload size={23}/></button></a>
+  //        </div>
+  //      </div>
+  //     </div>
      
     
-      </Container>
-     <Footer/>
-    </>
+  //     </Container>
+  //    <Footer/>
+  //   </>
     
-  )
+  // )
 }
 
 export default DetailsPage
